89c9af727fb67f63f9ad2ef849cf9db17b835763
sushantk-mhty <sushant.mhty@outlook.com>
2023-11-20 22:17:15 +0530

    add about section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
+  public age: number = 0;
+  public isEligibile: boolean = false;
 
+  public checkEligibility() {
+    this.isEligibile = this.age >= 18;
+  }
 }
